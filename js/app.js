642c1b4590b0630a32745abe95224dba9404cd79
angelafolz <angelafolz@gmail.com>
2014-04-05 17:05:14 -0500

    cool pose on "x" keydown

--- js/app.js
+++ js/app.js
@@ -9,19 +9,31 @@ $(document).ready(function(){
 	})
 	.mousedown(function(){
 		playHadouken();
-		$(".ryu-ready").hide();
+		$(".ryu-ready, .ryu-still, .ryu-cool").hide();
 		$(".ryu-throwing").show();
 		$(".hadouken").finish().show()
-			.animate({"left": "300px"},
-				500,
-				function(){
-					$(this).hide();
-					$(this).css({"left": "-210px"});
-				});
+		.animate({"left": "300px"},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css({"left": "-210px"});
+			});
 	})
 	.mouseup(function(){
 		$(".ryu-throwing").hide();
-		$(".ryu-ready").show();
+		$(".ryu-still").show();
+	});
+	$(document).keydown(function(event){
+		if(event.which == 88){
+			$(".ryu-still, .ryu-ready").hide();
+			$(".ryu-cool").show();
+		}
+	});
+	$(document).keyup(function(event){
+		if(event.which == 88){ // in case another key is pressed and released while holding x
+			$(".ryu-cool").hide();
+			$(".ryu-still").show();
+		}
 	});
 });
 
